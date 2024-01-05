c22e68df18bf7594094dd7b9475647fe2577345c
unknown <kalejaiyecaleb@gmail.com>
2024-01-05 01:07:44 +0100

    second commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    Footer,
+    PopularProducts,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe
+};
